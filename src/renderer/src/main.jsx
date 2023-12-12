8926bfbae9dfc630d3ab993e6a4d165449e8d035
Yaroslav Khakhuda <hahyda14@gmail.com>
2023-12-12 14:14:16 +0200

    add hash route

--- src/renderer/src/main.jsx
+++ src/renderer/src/main.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Container from './shared/componens/Container/Container.jsx';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './shared/styled/index.css';
 import '@fontsource/open-sans';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Container>
         <App />
       </Container>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
